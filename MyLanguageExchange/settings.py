239e131af7fcc303861f50c527e0e29ceaf441e7
SaliMerc <nyawadesaline@gmail.com>
2025-04-19 23:23:49 +0300

    modified requirements

--- MyLanguageExchange/settings.py
+++ MyLanguageExchange/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a)9bske)d23fx^vh2we8$an%)hj-cdr#mwozcz+n^siz35fah@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
