82fee11458d5e77f9eefa04b6924955fbafe4ba2
SaliMerc <nyawadesaline@gmail.com>
2025-04-19 23:44:28 +0300

    added whitenoise

--- MyLanguageExchange/settings.py
+++ MyLanguageExchange/settings.py
@@ -31,7 +31,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'mylanguageapp-o795.onrender.com'
+    'mylanguageapp-o795.onrender.com',
 ]
 
 # Application definition
@@ -54,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = 'MyLanguageExchange.urls'
@@ -121,6 +123,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
